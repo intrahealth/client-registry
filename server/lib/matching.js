e5edd3b0f9a006771e26ed7f035e4a12379e5c27
Ally Shaban <allyshaban5@gmail.com>
2019-12-27 15:46:17 +0300

    adding support for soundex and jaro-winkler algorithms

--- server/lib/matching.js
+++ server/lib/matching.js
@@ -1,6 +1,8 @@
 /* eslint-disable promise/param-names */
 const levenshtein = require('fast-levenshtein');
 const dlevenshtein = require('damerau-levenshtein');
+const jaroWankler = require('jaro-winkler');
+const soundex = require('soundex-code');
 const logger = require('./winston');
 const config = require('./config');
 const fhirWrapper = require('./fhir')();
@@ -25,7 +27,7 @@ module.exports = () => ({
       this.getMatches({
         sourceResource,
         targetResources,
-        ignoreList
+        ignoreList,
       },
       matched => {
         matches.entry = matches.entry.concat(matched.entry);
@@ -35,11 +37,13 @@ module.exports = () => ({
           this.performMatch({
             sourceResource,
             ignoreList,
-            url: next
-          }, matched => {
+            url: next,
+          },
+          matched => {
             matches.entry = matches.entry.concat(matched.entry);
             return callback(matches);
-          });
+          }
+          );
         } else {
           return callback(matches);
         }
@@ -59,8 +63,8 @@ module.exports = () => ({
     const promises = [];
     for (const targetResource of targetResources.entry) {
       promises.push(
-        new Promise((resolve) => {
-          const ignore = ignoreList.find((ignoreId) => {
+        new Promise(resolve => {
+          const ignore = ignoreList.find(ignoreId => {
             return ignoreId === targetResource.resource.id;
           });
           if (ignore) {
@@ -73,18 +77,29 @@ module.exports = () => ({
               new Promise(ruleResolve => {
                 const rule = decisionRules[ruleField];
                 const sourceValue = fhir.evaluate(sourceResource, rule.path);
-                const targetValue = fhir.evaluate(targetResource.resource, rule.path);
+                const targetValue = fhir.evaluate(
+                  targetResource.resource,
+                  rule.path
+                );
                 if (
                   !sourceValue ||
                   !targetValue ||
-                  (typeof sourceValue === 'object' && !Array.isArray(sourceValue)) ||
-                  (typeof targetValue === 'object' && !Array.isArray(targetValue))
+                  (typeof sourceValue === 'object' &&
+                    !Array.isArray(sourceValue)) ||
+                  (typeof targetValue === 'object' &&
+                    !Array.isArray(targetValue))
                 ) {
                   if (typeof sourceValue === 'object') {
-                    logger.warn('Object comparison are not supported ' + JSON.stringify(sourceValue));
+                    logger.warn(
+                      'Object comparison are not supported ' +
+                      JSON.stringify(sourceValue)
+                    );
                   }
                   if (typeof targetValue === 'object') {
-                    logger.warn('Object comparison are not supported ' + JSON.stringify(targetValue));
+                    logger.warn(
+                      'Object comparison are not supported ' +
+                      JSON.stringify(targetValue)
+                    );
                   }
                   missMatchFound = true;
                   return ruleResolve();
@@ -114,6 +129,12 @@ module.exports = () => ({
                     targetValue,
                     rule.threshold
                   );
+                  if (!isMatch) {
+                    missMatchFound = true;
+                  }
+                  break;
+                case 'jaro-winkler':
+                  isMatch = this.jaroWinklerMatcher(sourceValue, targetValue, rule.threshold);
                   if (!isMatch) {
                     missMatchFound = true;
                   }
@@ -262,6 +283,81 @@ module.exports = () => ({
     }
     return false;
   },
+
+  jaroWinklerMatcher (value1, value2, threshold) {
+    if (!Array.isArray(value1)) {
+      value1 = [value1];
+    }
+    if (!Array.isArray(value2)) {
+      value2 = [value2];
+    }
+    let array1;
+    let array2;
+    if (value1.length <= value2.length) {
+      array1 = value1;
+      array2 = value2;
+    } else {
+      array2 = value1;
+      array1 = value2;
+    }
+    let withinThreshold = true;
+    for (const val1 of array1) {
+      let score = false;
+      for (const val2 of array2) {
+        const thisScore = jaroWankler(val1, val2);
+        if (score === false || thisScore.steps < score) {
+          score = thisScore.steps;
+        }
+      }
+      if (score < threshold) {
+        withinThreshold = false;
+      }
+    }
+    if (withinThreshold) {
+      return true;
+    }
+    return false;
+  },
+
+  soundexMatcher (value1, value2) {
+    if (!Array.isArray(value1)) {
+      value1 = [value1];
+    }
+    if (!Array.isArray(value2)) {
+      value2 = [value2];
+    }
+    let array1;
+    let array2;
+    if (value1.length <= value2.length) {
+      array1 = value1;
+      array2 = value2;
+    } else {
+      array2 = value1;
+      array1 = value2;
+    }
+    let allMatches = true;
+    for (let val1 of array1) {
+      let matchFound = false;
+      for (let val2 of array2) {
+        val1 = val1.toLowerCase();
+        val1 = cleanValue(val1);
+        val2 = val2.toLowerCase();
+        val2 = cleanValue(val2);
+        const code1 = soundex(val1);
+        const code2 = soundex(val2);
+        if (code1 == code2) {
+          matchFound = true;
+        }
+      }
+      if (!matchFound) {
+        allMatches = false;
+      }
+    }
+    if (allMatches) {
+      return true;
+    }
+    return false;
+  }
 });
 
 function cleanValue (value) {
