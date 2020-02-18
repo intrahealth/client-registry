9ef8777977d42f108b741a91bbde2e5765fecaec
Ally Shaban <allyshaban5@gmail.com>
2020-02-18 08:17:05 +0300

    adding logging to a file

--- server/lib/winston.js
+++ server/lib/winston.js
@@ -1,14 +1,25 @@
 const winston = require('winston');
+require('winston-daily-rotate-file');
+
+var transport = new winston.transports.DailyRotateFile({
+  dirname: '/var/log',
+  filename: 'openclientregistry-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '10m',
+  maxFiles: '14d',
+});
+
 const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
+  transports: [new winston.transports.Console(), transport],
   format: winston.format.combine(
     winston.format.colorize({
-      all: true
+      all: true,
     }),
     winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
+      format: 'YYYY-MM-DD HH:mm:ss',
     }),
     winston.format.simple()
-  )
+  ),
 });
 module.exports = logger;
