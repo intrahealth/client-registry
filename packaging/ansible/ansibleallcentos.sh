fb669eb7c94f8a98aa007ce4502db1cd83468c6d
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2020-03-31 19:44:17 -0400

    rebuild ansible scripts

--- packaging/ansible/ansibleallcentos.sh
+++ packaging/ansible/ansibleallcentos.sh
@@ -4,6 +4,10 @@ set -ex
 # --forks 1 to stop hosts checking on first run: https://github.com/ansible/ansible/issues/25068
 ansible-playbook -i /usr/local/etc/ansible/hosts user.yaml --forks 1
 ansible-playbook -i /usr/local/etc/ansible/hosts prep_centos.yaml -e user=opencr
+ansible-playbook -i /usr/local/etc/ansible/hosts elasticsearch.yaml -e user=opencr
+ansible-playbook -i /usr/local/etc/ansible/hosts postgres.yaml -e user=opencr
+ansible-playbook -i /usr/local/etc/ansible/hosts hapifhir.yaml -e user=opencr
 # ansible-playbook -i /usr/local/etc/ansible/hosts install.yaml -e user=opencr
 # ansible-playbook -i /usr/local/etc/ansible/hosts services.yaml -e user=opencr
 # ansible-playbook -i /usr/local/etc/ansible/hosts troubleshoot.yaml -e user=opencr
+ 
