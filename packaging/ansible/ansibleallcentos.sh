21226409f79b5f4e598ec965082b6311891823f8
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2020-04-01 19:47:37 -0400

    rebuild ansible scripts with tomcat

--- packaging/ansible/ansibleallcentos.sh
+++ packaging/ansible/ansibleallcentos.sh
@@ -5,6 +5,7 @@ set -ex
 ansible-playbook -i /usr/local/etc/ansible/hosts user.yaml --forks 1
 ansible-playbook -i /usr/local/etc/ansible/hosts prep_centos.yaml -e user=opencr
 ansible-playbook -i /usr/local/etc/ansible/hosts elasticsearch.yaml -e user=opencr
+ansible-playbook -i /usr/local/etc/ansible/hosts tomcat.yaml -e user=opencr
 ansible-playbook -i /usr/local/etc/ansible/hosts postgres.yaml -e user=opencr -e pgpass=hapi
 ansible-playbook -i /usr/local/etc/ansible/hosts hapi.yaml -e user=opencr
 # ansible-playbook -i /usr/local/etc/ansible/hosts opencr.yaml -e user=opencr
