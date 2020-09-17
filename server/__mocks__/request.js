f60dbba564753f064077a3d2c9c6c9d6a249f157
Luke Duncan <lduncan@intrahealth.org>
2020-09-17 11:07:24 -0600

    Added in POST/PUT/DELETE methods for the request mock.

--- server/__mocks__/request.js
+++ server/__mocks__/request.js
@@ -21,7 +21,7 @@ function __setFhirResults( url, data, newFhirResults ) {
 
 function __setFhirError( url, data, status, newFhirResults ) {
   let objHash = __hashObject( data )
-  fhirErrors[ url + objHash ] = { data: newFhirResults, status: status }
+  fhirErrors[ url + objHash ] = { data: newFhirResults, statusCode: status }
 }
 
 function get( options, callback ) {
@@ -34,46 +34,36 @@ function get( options, callback ) {
   }
 }
 
-function put( url, data, config ) {
-  let objHash = __hashObject( data )
-  /* Needs to be redone for request results 
-  return new Promise( (resolve, reject) => {
-    if ( fhirErrors.hasOwnProperty( url + objHash ) ) {
-      reject( { response: fhirErrors[ url + objHash ] } )
-    } else {
-      resolve( { data: fhirResults[ url + objHash ], status: 200 } )
-    }
-  } )
-  */
+function put( options, callback ) {
+  let url = options.url
+  let objHash = __hashObject( options.json )
+  if ( fhirErrors.hasOwnProperty( url + objHash ) ) {
+    callback( fhirErrors[ url + objHash ] )
+  } else {
+    callback( null, { statusCode: 200 }, fhirResults[ url + objHash ] )
+  }
 }
 
-function post( url, data, config ) {
-  let objHash = __hashObject( data )
-  /* Needs to be redone for request results 
-  return new Promise( (resolve, reject) => {
-    if ( fhirErrors.hasOwnProperty( url + objHash ) ) {
-      reject( { response: fhirErrors[ url + objHash ] } )
-    } else {
-      let response = { ...fhirResults[ url + objHash ] }
-      // overwrite the id like the server would on a create (or add it)
-      response.id = "1"
-      resolve( { data: response, status: 201 } )
-    }
-  } )
-  */
+function post( options, callback ) {
+  let url = options.url
+  let objHash = __hashObject( options.json )
+  if ( fhirErrors.hasOwnProperty( url + objHash ) ) {
+    callback( fhirErrors[ url + objHash ] )
+  } else {
+    let response = { ...fhirResults[ url + objHash ] }
+    response.id = "1"
+    callback( null, { statusCode: 201 }, response )
+  }
 }
 
 function remove( url, config ) {
-  let paramHash = __hashObject( config.params )
-  /* Needs to be redone for request results 
-  return new Promise( (resolve, reject) => {
-    if ( fhirErrors.hasOwnProperty( url + paramHash ) ) {
-      reject( { response: fhirErrors[ url + paramHash ] } )
-    } else {
-      resolve( { data: fhirResults[ url + paramHash ], status: 200 } )
-    }
-  } )
-  */
+  let url = options.url
+  let paramHash = __hashObject( null )
+  if ( fhirErrors.hasOwnProperty( url + paramHash ) ) {
+    callback( fhirErrors[ url + paramHash ] )
+  } else {
+    callback( null, { statusCode: 200 }, fhirResults[ url + paramHash )
+  }
 }
 
 request.__setFhirResults = __setFhirResults
