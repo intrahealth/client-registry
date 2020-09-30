1efb124ae1ed272782463348e06112de4c3fe715
Luke Duncan <lduncan@intrahealth.org>
2020-09-30 08:40:06 -0600

    Removed the restriction to only test cucumber steps tests.

--- server/jest.config.js
+++ server/jest.config.js
@@ -142,9 +142,6 @@ module.exports = {
   //   "**/__tests__/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
-  testMatch: [
-    "**/*.steps.js"
-  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
