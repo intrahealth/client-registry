a0136cd3f386e842037c12b074947f2c390fa63b
Ally Shaban <allyshaban5@gmail.com>
2020-12-14 09:42:00 +0300

    cache ES after loading patients tests data

--- server/__tests__/e2e/resetTestData.js
+++ server/__tests__/e2e/resetTestData.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const URI = require('urijs');
 const config = require('../../lib/config');
 const logger = require('../../lib/winston');
+const cacheFHIR = require('../../lib/tools/cacheFHIR');
 const patientsBundle = require('./fhirresources/patientsBundle.json');
 
 logger.info("Adding default patients data used for running tests");
@@ -23,6 +24,14 @@ axios(options).then((resp) => {
   if (resp.status < 200 || resp.status > 299) {
     logger.error(JSON.stringify(resp, 0, 2));
   } else {
+    const runsLastSync = config.get("sync:lastFHIR2ESSync");
+    cacheFHIR.fhir2ES({
+      lastSync: runsLastSync
+    }, (err) => {
+      if(err) {
+        logger.error(err);
+      }
+    });
     logger.info('Default patients data added successfully');
   }
 }).catch((err) => {
